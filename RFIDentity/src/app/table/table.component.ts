1a84040f0c1e0de3937d92e1c784aecdef88e90a
Kemnaz <117764641+Kemnaz@users.noreply.github.com>
2024-08-08 13:42:37 +0200

    added api calling and data filtering

--- RFIDentity/src/app/table/table.component.ts
+++ RFIDentity/src/app/table/table.component.ts
@@ -1,21 +1,46 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { Asset } from './table.module';
 import { assets } from './DummyAssets';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { HttpClient } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+
 const ELEMENT_DATA: Asset[] = assets;
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent implements AfterViewInit {
+export class TableComponent implements AfterViewInit, OnInit {
   dataSource = new MatTableDataSource<Asset>(ELEMENT_DATA);
+  filterValue = '';
+
   displayedColumns: string[] = [
     'AssetId',
     'Description',
@@ -25,17 +50,41 @@ export class TableComponent implements AfterViewInit {
     'Action',
   ];
 
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
   constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
   @ViewChild(MatSort) sort!: MatSort;
-
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit() {
+    const subscription = this.httpClient
+      .get<Asset[]>('https://66b4810f9f9169621ea33918.mockapi.io/rfid/assets')
+      .subscribe({
+        next: (resData) => {
+          this.dataSource.data = resData; // Assign the response data directly to the dataSource
+          console.log(resData);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    // console.log(this.dataSource.filter);
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
   /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
     // This example uses English messages. If your application supports
