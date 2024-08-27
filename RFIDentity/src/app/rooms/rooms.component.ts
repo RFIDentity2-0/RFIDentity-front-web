1bf158b0973288cfbbab5f4a3b02d5da6f5f00ad
Kemnaz <117764641+Kemnaz@users.noreply.github.com>
2024-08-27 15:34:21 +0200

    room pagination WIP

--- RFIDentity/src/app/rooms/rooms.component.ts
+++ RFIDentity/src/app/rooms/rooms.component.ts
@@ -21,6 +21,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { InventoryService } from '../services/inventory.service';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 // Create sample assets
 
 @Component({
@@ -32,6 +33,7 @@ import { HttpClient } from '@angular/common/http';
     FormsModule,
     MatFormFieldModule,
     MatSelectModule,
+    CommonModule,
   ],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss',
@@ -59,11 +61,12 @@ export class RoomsComponent implements OnInit {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<DataStructure>(
-        `http://localhost:8080/api/locations/locationsToRoomsPage?page=0&size=5&sort=`
+        `http://localhost:8080/api/locations/locationsToRoomsPage?page=${page}&size=${size}&sort=`
       )
       .subscribe({
         next: (resData) => {
           this.datasource = resData;
+          this.totalItems = resData.totalElements;
         },
         complete: () => this.isFetching.set(false),
       });
@@ -71,6 +74,11 @@ export class RoomsComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+  // Pagination variables
+  pageSizes = [1, 2, 5, 10, 15];
+  totalItems = 0;
+  pageIndex = 0;
+  pageSize = this.pageSizes[0];
 
   // ----------------------------checkbox logic---------------------------
   readonly room = signal<RoomSelection>({
@@ -108,4 +116,39 @@ export class RoomsComponent implements OnInit {
   }
 
   // ---------------------------------------------------------------------
+
+  // Pagination logic
+  // Custom pagination logic
+  onNextPage() {
+    if ((this.pageIndex + 1) * this.pageSize < this.totalItems) {
+      this.pageIndex++;
+      this.fetchRoomData(this.pageIndex, this.pageSize);
+    }
+  }
+
+  onPreviousPage() {
+    if (this.pageIndex > 0) {
+      this.pageIndex--;
+      this.fetchRoomData(this.pageIndex, this.pageSize);
+    }
+  }
+
+  onPageSizeChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.pageSize = Number(selectElement.value);
+    this.pageIndex = 0; // Reset to first page
+    this.fetchRoomData(this.pageIndex, this.pageSize);
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
+  }
+
+  canGoNext(): boolean {
+    return (this.pageIndex + 1) * this.pageSize < this.totalItems;
+  }
+
+  canGoPrevious(): boolean {
+    return this.pageIndex > 0;
+  }
 }
